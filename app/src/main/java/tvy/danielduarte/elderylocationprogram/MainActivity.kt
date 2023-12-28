bfe69ca1a9fdc78c1ffa4a7fb5a8ebf24c6d056c
DanielDuarte <danielduarte123004@gmail.com>
2023-12-28 12:27:36 +0000

    Refactor the main activity and delete the other main activity

--- app/src/main/java/tvy/danielduarte/elderylocationprogram/MainActivity.kt
+++ app/src/main/java/tvy/danielduarte/elderylocationprogram/MainActivity.kt
@@ -7,57 +7,61 @@ import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.MapView
-
-
 class MainActivity : AppCompatActivity() {
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private val DISTANCE_IN_RADIUS_METERS = 15
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient // The Client which is used to get the device location
+    private val REQUEST_LOCATION_PERMISSION = 1 // Integer of the request used by the OS to distinct mutiple requests
+    private var currentLocation : Location ?= null // "Location" type object, it's the current location updated once the app is created
+    private var center : Location ?= null // Center of the perimeter
+    private var radius : Float ?= null // Radius of the perimeter
     private lateinit var textView: TextView
-
-    val perimeterLatitudeStart = 39.2209175
-    val perimeterLongitudeStart = -8.686665
-    private var lastKnownLocation: Location? = null
-    private val REQUEST_LOCATION_PERMISSION = 1
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.textView)
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        /*
-        val map = findViewById<MapView>(R.id.mapView)
-
-        val mapCallback = CreateMap()
-        map.getMapAsync(mapCallback)
-         */
-
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this) // Initializing the client
+        textView = findViewById<TextView>(R.id.textView);
+        center = Location("provider")
+        center?.latitude = 39.2209175
+        center?.longitude = -8.686665
+        StartApp()
     }
 
-    fun Click(view: View) {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            startLocationUpdates()
-        }else {
-            // Solicitar permissão
+    //Checks if the app doesn't have the necessary permissions, if not, asks for them, then gets the current location
+    private fun StartApp(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION ),REQUEST_LOCATION_PERMISSION )
         }
+        startLocationUpdates()
     }
+
     private val locationCallback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
             super.onLocationResult(locationResult)
             if (locationResult.lastLocation != null) {
-                lastKnownLocation = locationResult.lastLocation
-                checkIfOutsidePerimeter()
+                currentLocation = locationResult.lastLocation
+                if(currentLocation != null && center != null){
+                    distanceFromCenter(currentLocation!!, center!!)
+                }
             }
         }
     }
@@ -66,49 +70,40 @@ class MainActivity : AppCompatActivity() {
         val locationRequest = LocationRequest.create()
             .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
             .setInterval(5000) // Intervalo em milissegundos para atualizações de localização (aqui, a cada 10 segundos)
-            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
 
     }
+    // Function called by the click of "btnCenter" sets the current device location to be the center of the perimeter in the "center" variable
+    fun setPerimeterCenter(view: View) {
+        center?.latitude = 39.2209175
+        center?.longitude = -8.686665//Por ver
+    }
 
-    private fun checkIfOutsidePerimeter() {
-        // Verifique se a última localização conhecida está fora do perímetro desejado
-        if (lastKnownLocation != null) {
-            val latitude = lastKnownLocation!!.latitude
-            val longitude = lastKnownLocation!!.longitude
+    // Function called by the click of "btnRadius" sets the radius of the perimeter from the EditText "txtRadius" and gives the value to the "radius" variable
 
-            textView.text = "Latitude: $latitude, Longitude: $longitude"
+    fun setPerimeterRadius(view: View) {
+        val txtRadius : EditText = findViewById(R.id.txtRadius)
+        radius = txtRadius.text.toString().toFloat()
+    }
 
-
-            val distanceAtual = calculateDistance(perimeterLatitudeStart, perimeterLongitudeStart,latitude, longitude)
-            Log.d("ABC1",distanceAtual.toString())
-
-
-            if (distanceAtual > DISTANCE_IN_RADIUS_METERS) {
-                // O dispositivo está fora do perímetro, notifique o usuário
-                textView.text = "Fora do perímetro! Distância: $distanceAtual metros"
-            } else {
-                // O dispositivo está dentro do perímetro
-                textView.text = "Dentro do perímetro"
-            }
-
-
-
+    // Checks the distance of the device from the center CURRENTLY UNIMPLEMMENTED needs a timer for it to be called from time to time same with the method get current location
+    private fun distanceFromCenter(currentLocation: Location, center: Location)  {
+        if(radius != null){
+            return checkIfOutOfBounds(currentLocation.distanceTo(center), radius!!)
         }
     }
-    private fun calculateDistance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Float {
-        val location1 = Location("")
-        location1.latitude = lat1
-        location1.longitude = lon1
 
-        val location2 = Location("")
-        location2.latitude = lat2
-        location2.longitude = lon2
+    // Checks if the device is out of the perimeter
 
-        return location1.distanceTo(location2)
-    }
+    private fun checkIfOutOfBounds(distance: Float, radius: Float){
+        if (distance > radius){
+            // Ligar ao controlo de animais o crocodilo fugiu da jaula
 
-    override fun onDestroy() {
-        super.onDestroy()
-        fusedLocationClient.removeLocationUpdates(locationCallback)
+             textView.text = "Ligar ao controlo de animais o crocodilo fugiu da jaula"
+
+        }else{
+            textView.text = "Desligar ao controlo de animais o crocodilo fugiu da jaula"
+        }
+
     }
 }
