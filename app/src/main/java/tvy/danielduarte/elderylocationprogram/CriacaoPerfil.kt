880a896b80523b6a8532d3589ba2cf80c0f0e2d7
TMTomas <126092390+TMTomas@users.noreply.github.com>
2024-01-08 16:26:06 +0000

    Classe para criar perfil

---
+++ app/src/main/java/tvy/danielduarte/elderylocationprogram/CriacaoPerfil.kt
@@ -0,0 +1,11 @@
+package tvy.danielduarte.elderylocationprogram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CriacaoPerfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_criacao_perfil)
+    }
+}
