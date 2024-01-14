954af0e682776c208afef40e4cfb8c76008f5c56
TMTomas <126092390+TMTomas@users.noreply.github.com>
2024-01-14 14:37:47 +0000

    Add Contact and Email to ProfileObj

--- app/src/main/java/tvy/danielduarte/elderylocationprogram/classes/ProfileObj.kt
+++ app/src/main/java/tvy/danielduarte/elderylocationprogram/classes/ProfileObj.kt
@@ -2,14 +2,17 @@ package tvy.danielduarte.elderylocationprogram
 
 import android.graphics.Bitmap
 import android.location.Location
+import android.provider.ContactsContract.CommonDataKinds.Email
 import tvy.danielduarte.elderylocationprogram.classes.GeoFenceObj
 import tvy.danielduarte.elderylocationprogram.classes.NotificationTypeObj
 import java.io.Serializable
 
-class ProfileObj (Name: String, Image: Bitmap, GeoFenceObj: GeoFenceObj, NotificationType: NotificationTypeObj):
+class ProfileObj (Name: String, Image: Bitmap, Contact: Int, Email:String, GeoFenceObj: GeoFenceObj, NotificationType: NotificationTypeObj):
     Serializable {
     var name: String = Name
     var image: Bitmap = Image
+    var contact: Int = Contact
+    var email: String = Email
     var geoFenceObj: GeoFenceObj = GeoFenceObj
     var notificationType: NotificationTypeObj = NotificationType
 
