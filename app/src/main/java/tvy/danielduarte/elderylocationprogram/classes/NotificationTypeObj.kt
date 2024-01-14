b1e683e51a6177ca448dfd9f472d192766d0f9b3
TMTomas <126092390+TMTomas@users.noreply.github.com>
2024-01-14 14:38:28 +0000

    Logica de mandar mails

--- app/src/main/java/tvy/danielduarte/elderylocationprogram/classes/NotificationTypeObj.kt
+++ app/src/main/java/tvy/danielduarte/elderylocationprogram/classes/NotificationTypeObj.kt
@@ -1,31 +1,44 @@
 package tvy.danielduarte.elderylocationprogram.classes
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.content.Context
-import android.os.Build
-import androidx.core.content.ContextCompat.getSystemService
+import android.content.Intent
+import android.net.Uri
+import android.provider.ContactsContract.CommonDataKinds.Contactables
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 
 
-class NotificationTypeObj(Message:Boolean, Email:Boolean, Notification:Boolean) {
-    var message:Boolean = Message
-    var email:Boolean = Email
-    var notification:Boolean = Notification
+class NotificationTypeObj(SendMessage:Boolean, SendEmail:Boolean, SendNotification:Boolean, Contact: Int, Email: String) {
+
+    var contact:Int = Contact
+    var email:String = Email
+
+    private var sendMessage:Boolean = SendMessage
+    private var sendEmail:Boolean = SendEmail
+    private var sendNotification:Boolean = SendNotification
 
     fun sendMessage(){
-        if (message == true){
+        if (sendMessage == true){
 
         }
     }
 
-    fun sendEmail(){
-        if (email == true){
+    fun sendEmail(context: Context, recipient: String, subject: String, message: String){
+        if (sendEmail == true){
+            val intent = Intent(Intent.ACTION_SEND).apply{
+                type = "text/plain"
+                putExtra(Intent.EXTRA_EMAIL, recipient)
+                putExtra(Intent.EXTRA_SUBJECT, subject)
+                putExtra(Intent.EXTRA_TEXT, message)
+
+            }
+            context.startActivity(intent)
 
         }
     }
 
     fun sendNotification(message:String, context: Context){
-        if (notification == true){
+        if (sendNotification == true){
             var notificationService = NotificationService(context)
             notificationService.showNotification(message)
         }
